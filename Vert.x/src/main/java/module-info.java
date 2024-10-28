120c4ab82e4e71c2e6375a64b2b270471d0e692b
GedMarc <marc.magon@gmail.com>
2024-10-28 03:34:44 +0200

    Build updates for intellij bug on optimize imports java

--- Vert.x/src/main/java/module-info.java
+++ Vert.x/src/main/java/module-info.java
@@ -1,11 +1,11 @@
-import com.guicedee.guicedinjection.interfaces.IGuiceModule;
 import com.guicedee.vertx.spi.VertxHttpServerConfigurator;
 import com.jwebmp.vertx.JWebMPVertx;
 import com.jwebmp.vertx.implementations.JWebMPVertxBinder;
+import com.guicedee.guicedinjection.interfaces.*;
 
 module com.jwebmp.vertx {
     requires transitive com.jwebmp.client;
-    requires io.vertx;
+    requires io.vertx.core;
     requires guiced.vertx;
     requires static lombok;
     requires org.apache.commons.lang3;
@@ -13,6 +13,6 @@ module com.jwebmp.vertx {
     opens com.jwebmp.vertx.implementations to com.google.guice;
     opens com.jwebmp.vertx to com.google.guice;
 
-    provides IGuiceModule with JWebMPVertx,JWebMPVertxBinder;
+    provides IGuiceModule with JWebMPVertx, JWebMPVertxBinder;
     provides VertxHttpServerConfigurator with JWebMPVertx;
 }
